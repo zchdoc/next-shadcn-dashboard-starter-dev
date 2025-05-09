14edadac3bcc98db69b073cc95f5827091305fee
zchcpy <571928489@qq.com>
2025-05-09 10:41:54 +0800

    feat(layout): 更新网站元数据和图标
    - 设置网站标题为 "Z1.Tool"
    - 添加网站图标
    - 根据环境动态设置标题前缀
    - 更新网站描述

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,10 +4,20 @@ import Header from '@/components/layout/header';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import type { Metadata } from 'next';
 import { cookies } from 'next/headers';
-
+import { headers } from 'next/headers';
+const title = 'Z1.Tool';
 export const metadata: Metadata = {
-  title: 'Next Shadcn Dashboard Starter',
-  description: 'Basic dashboard with Next.js and Shadcn'
+  title: title,
+  description:
+    'Build tool box site with Basic dashboard with Next.js and Shadcn',
+  icons: {
+    icon: [
+      {
+        url: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>👋</text></svg>',
+        type: 'image/svg+xml'
+      }
+    ]
+  }
 };
 
 export default async function DashboardLayout({
@@ -18,7 +28,7 @@ export default async function DashboardLayout({
   // Persisting the sidebar state in the cookie.
   const cookieStore = await cookies();
   const sidebarState = cookieStore.get('sidebar_state'); // Changed from sidebar:state to sidebar_state
-  console.log('sidebarState', sidebarState, typeof sidebarState);
+  // console.log('sidebarState', sidebarState, typeof sidebarState)
   let defaultOpen;
   if ('undefined' === typeof sidebarState) {
     defaultOpen = false;
@@ -26,7 +36,21 @@ export default async function DashboardLayout({
     defaultOpen = sidebarState?.value !== 'false';
   }
 
-  console.log('defaultOpen', defaultOpen);
+  // console.log('defaultOpen', defaultOpen)
+
+  const headersList = await headers();
+  const host = headersList.get('host');
+  const isDev = host?.startsWith('localhost') || host?.startsWith('127.0.0.1');
+  const currentTitle = isDev ? `Dev-${title}` : title;
+
+  if (
+    metadata.title &&
+    typeof metadata.title === 'object' &&
+    'default' in metadata.title
+  ) {
+    metadata.title.default = currentTitle;
+  }
+
   return (
     <KBar>
       <SidebarProvider defaultOpen={defaultOpen}>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import ThemeProvider from '@/components/layout/ThemeToggle/theme-provider';
 import { cn } from '@/lib/utils';
 import type { Metadata, Viewport } from 'next';
 import { cookies } from 'next/headers';
+import { headers } from 'next/headers';
 import NextTopLoader from 'nextjs-toploader';
 import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import './globals.css';
@@ -14,10 +15,19 @@ const META_THEME_COLORS = {
   light: '#ffffff',
   dark: '#09090b'
 };
-
+const title = 'Z1.Tool';
 export const metadata: Metadata = {
-  title: 'Next Shadcn',
-  description: 'Basic dashboard with Next.js and Shadcn'
+  title: title,
+  description:
+    'Build tool box site with Basic dashboard with Next.js and Shadcn',
+  icons: {
+    icon: [
+      {
+        url: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>👋</text></svg>',
+        type: 'image/svg+xml'
+      }
+    ]
+  }
 };
 
 export const viewport: Viewport = {
@@ -32,6 +42,18 @@ export default async function RootLayout({
   const cookieStore = await cookies();
   const activeThemeValue = cookieStore.get('active_theme')?.value;
   const isScaled = activeThemeValue?.endsWith('-scaled');
+  const headersList = await headers();
+  const host = headersList.get('host');
+  const isDev = host?.startsWith('localhost') || host?.startsWith('127.0.0.1');
+  const currentTitle = isDev ? `Dev-${title}` : title;
+
+  if (
+    metadata.title &&
+    typeof metadata.title === 'object' &&
+    'default' in metadata.title
+  ) {
+    metadata.title.default = currentTitle;
+  }
 
   return (
     <html lang='en' suppressHydrationWarning>
